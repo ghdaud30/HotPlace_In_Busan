9cfcc60eb1ab4c18cec2c330d28057336ea02349
ghdaud30 <54072339+ghdaud30@users.noreply.github.com>
2023-11-22 17:12:22 +0900

    DB에서 맛집 정보 영어 데이터 가져오는 Test

--- HotPlace_Busan/src/test/java/busan/food/HotPlaceBusanApplicationTests.java
+++ HotPlace_Busan/src/test/java/busan/food/HotPlaceBusanApplicationTests.java
@@ -1,13 +1,30 @@
 package busan.food;
 
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+
+import busan.food.domain.GetFoodEng;
+import busan.food.repogitory.GetFoodEngRepository;
 
 @SpringBootTest
 class HotPlaceBusanApplicationTests {
 
+	@Autowired
+	GetFoodEngRepository getFoodEngRepository;
+	
 	@Test
-	void contextLoads() {
+	@Order(1)
+	@DisplayName("DB에서 맛집 정보 영어 데이터 가져오기")
+	void DBdata() {
+		List<GetFoodEng> foodlist = getFoodEngRepository.findAll();
+		for(GetFoodEng food : foodlist) {
+			System.out.println(food.getUcSeq());
+		}
 	}
-
+	
 }

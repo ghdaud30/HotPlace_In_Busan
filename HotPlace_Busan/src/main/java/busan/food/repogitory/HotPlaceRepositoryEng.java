e1796aa3c3637fcf1ec6b5a50a5c51ee5ca36933
ghdaud30 <54072339+ghdaud30@users.noreply.github.com>
2023-11-22 15:28:45 +0900

    json 파일 삭제 csv 사용 해서 DB 삽입

--- HotPlace_Busan/src/main/java/busan/food/repogitory/HotPlaceRepositoryEng.java
+++
@@ -1,9 +0,0 @@
-package busan.food.repogitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import busan.food.domain.HotPlaceEng;
-
-public interface HotPlaceRepositoryEng  extends JpaRepository<HotPlaceEng, Integer>{
-
-}
--- HotPlace_Busan/src/main/java/busan/food/repogitory/HotPlaceRepositoryKor.java
+++
@@ -1,9 +0,0 @@
-package busan.food.repogitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import busan.food.domain.HotPlaceKor;
-
-public interface HotPlaceRepositoryKor extends JpaRepository<HotPlaceKor, Integer>{
-
-}

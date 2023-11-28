8778b3b75f74746c872cbe87f0bc6101e536d57f
ghdaud30 <ghdaud3093@gmail.com>
2023-11-28 16:17:38 +0900

    웹사이트 유저 정보 저장
    회원가입시 유저 날짜 정보를 업데이트 하였습니다

--- HotPlace_Busan/src/main/java/busan/food/domain/Member.java
+++ HotPlace_Busan/src/main/java/busan/food/domain/Member.java
@@ -1,9 +1,15 @@
 package busan.food.domain;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,4 +32,9 @@ public class Member {
 	private String nickname;
 	private String telephone;
 	private String email;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	@Builder.Default
+	private Date date = new Date();
 }
